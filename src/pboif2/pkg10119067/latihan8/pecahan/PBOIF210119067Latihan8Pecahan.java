4377e12a90bdfd972b2a510228bd5c6a100919e5
dankakerllac <72844005+dankakerllac@users.noreply.github.com>
2020-10-19 22:47:01 +0700

    Update PBOIF210119067Latihan8Pecahan.java

--- src/pboif2/pkg10119067/latihan8/pecahan/PBOIF210119067Latihan8Pecahan.java
+++ src/pboif2/pkg10119067/latihan8/pecahan/PBOIF210119067Latihan8Pecahan.java
@@ -1,14 +1,19 @@
-/*
+/*/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pboif2.pkg10119067.latihan8.pecahan;
+package pboif2.pkg10119067;
 
 /**
  *
- * @author User
+ *
+ * NAMA     : Andreas Danhago
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi Program : Deklarasi & Pengaksesan
  */
+public class PBOIF210119067 {
 public class PBOIF210119067Latihan8Pecahan {
 
     /**
