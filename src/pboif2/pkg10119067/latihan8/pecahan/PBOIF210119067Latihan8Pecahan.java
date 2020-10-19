8df3a4e23b5326cc5e950dc1c0c755a4600e5fdf
User <User@DESKTOP-6N6EHH6>
2020-10-19 22:36:27 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan8/pecahan/PBOIF210119067Latihan8Pecahan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan8.pecahan;
+
+/**
+ *
+ * @author User
+ */
+public class PBOIF210119067Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
